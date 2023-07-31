8e64bd78843a7a78a4dc9236e66eef381f568346
Bruna Ferreira <bferreira.slv@gmail.com>
2023-07-31 18:11:27 +0000

    DEV: adjusted code format

--- bot.py
+++ bot.py
@@ -1,33 +1,32 @@
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from botcity.maestro import *
-
 from selenium.webdriver.firefox.service import Service
-from time import sleep
 
 
 def main():
     try:
-        # Setando o caminho do WebDriver do Firefox
+        # Seta o caminho do WebDriver do Firefox
         service = Service(executable_path=r"geckodriver.exe")
 
-        # Código utilizando Selenium puro, sem nenhuma dependência do framework BotCity 
+        # Cria uma instância do Navegador
         bot = webdriver.Firefox(service=service)
 
-        # Acessando página Practice Test Automation
+        # Acessa página Practice Test Automation
         bot.get("https://practicetestautomation.com/practice-test-login/")
 
-        # Buscando pelo elemento input de nome de usuário
+        # Busca pelo elemento input de nome de usuário
         input_username = bot.find_element(By.ID, "username")
         # Ação de digitar
         input_username.send_keys("student")
 
-        # Buscando pelo elemento input de senha
+        # Busca pelo elemento input de senha
         input_password = bot.find_element(By.ID, "password")
         # Ação de digitar
         input_password.send_keys("Password123")
 
-        # Buscando pelo elemento botão submit
+        # Busca pelo elemento botão submit
         input_button = bot.find_element(By.ID, "submit")
         # Ação de clicar
         input_button.click()
@@ -37,7 +36,8 @@ def main():
 
         # Busca pela confirmação de login
         logged = bot.find_element(By.CSS_SELECTOR, ".post-title")
-        print(logged.text)        
+        # Imprime o texto da confirmação
+        print(logged.text)
 
         # Busca pelo elemento botão log out
         logout = bot.find_element(By.CSS_SELECTOR, ".wp-block-button__link")
@@ -50,13 +50,18 @@ def main():
     except Exception as ex:
         # Busca pelo elemento de mensagem de erro
         error_alert = bot.find_element(By.ID, "error")
+
+        # Imprime a mensagem de erro e stacktrace
         print(error_alert.text)
         print(ex)
 
     finally:
         # Finaliza fechando o navegador
         bot.quit()
+
+        # Imprime mensagem de finalização
         print("Finally")
+
 
 if __name__ == "__main__":
     main()
