feca660c1bb8c9e1f43c5505c75c452d9e63ecef
Bruna Ferreira <51390917+bugelseif@users.noreply.github.com>
2023-08-15 16:35:57 +0000

    DEV: adjusted build file

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-$exclude = @("venv", "bot-cotacao.zip")
+$exclude = @("venv", "bot-selenium.zip")
 $files = Get-ChildItem -Path . -Exclude $exclude
 Compress-Archive -Path $files -DestinationPath "bot-selenium.zip" -Force
